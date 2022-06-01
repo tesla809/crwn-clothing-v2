0196e26e2737af6c578fd684a58445c2286f6ad6
Anthony Albertorio <anthony.albertorio@gmail.com>
2022-06-01 16:10:17 -0400

    created and styled navigation bar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,52 @@
-import { Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import "./navigation.styles.scss";
 
 const Navigation = () => {
   return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
-    </div>
+    </Fragment>
   );
 };
 
 export default Navigation;
+
+/*
+Can use fragments instead of divs
+a component that renders to nothing 
+when its mounted to the DOM.
+
+Useful to get around rule that component
+needs only one top level parent.
+
+Fragments are useful if you don't want to render
+an HTML element.
+
+Gets rid of wrapping div.
+
+Short hand - <></>
+
+React Router
+Link 
+- like an anchor tag, but uses the right browser
+that you are using.
+
+Importing SVGs
+- SVGs can be imported at React Components
+- SVGs are scalable vector graphics, 
+HTML that no matter the size, renders crisp images
+*/
